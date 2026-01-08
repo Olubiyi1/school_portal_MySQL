58457c69d83d2ce253c8de32e262dbc5210ae61f
olubiyi babajide <olubiyibabajide@gmail.com>
2026-01-08 23:41:34 +0100

    folder structure started and vaidation-message-created

---
+++ utils/validationMessage.js
@@ -0,0 +1,27 @@
+export const validationMessages = {
+  firstName: {
+    "any.required": "Please enter firstname",
+    "string.empty": "Firstname cannot be empty",
+    "string.min": "Firstname must be at least 3 characters long",
+    "string.max": "Firstname cannot exceed 50 characters",
+  },
+  lastName: {
+    "any.required": "Please enter surname",
+    "string.empty": "Surname cannot be empty",
+    "string.min": "surname must be at least 3 characters long",
+    "string.max": "surname cannot exceed 50 characters",
+  },
+  email: {
+    "any.required": "Please enter email",
+    "string.empty": "Email cannot be empty",
+    "string.email": "Please enter a valid email address",
+  },
+  password: {
+    "any.required": "Please enter a password",
+    "string.empty": "Password cannot be empty",
+    "string.min": "Password must be at least 8 characters long",
+    "string.max": "Password cannot exceed 30 characters",
+    "string.pattern.base":"Password must include uppercase, lowercase, number, and special character",
+  }
+};
+
